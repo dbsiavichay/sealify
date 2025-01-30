7cddf66e5d0769ee293e35e0c7d16317a40ea769
Denis Siavichay <dbsiavichay@gmail.com>
2025-01-30 15:31:47 -0500

    feat: cipher cert info

--- src/certificate/infra/repositories.py
+++ src/certificate/infra/repositories.py
@@ -4,15 +4,20 @@ from typing import List
 from src.certificate.domain.models import Certificate
 from src.certificate.domain.repositories import CertificateRepository
 from src.core.app.exceptions import ResourceNotFoundException
+from src.core.app.utils import CipherUtil
 from src.core.infra.aws_clients import AWSDynamoDBClient
 
 
 class CertificateRepositoryDynamoDB(CertificateRepository):
-    def __init__(self, db_client: AWSDynamoDBClient):
+    def __init__(self, db_client: AWSDynamoDBClient, cipher_util: CipherUtil) -> None:
         self.db_client = db_client
+        self.cipher_util = cipher_util
 
     def save(self, certificate: Certificate) -> None:
-        self.db_client.put_item(certificate.dict())
+        cert = self.cipher_util.encrypt(certificate.cert)
+        key = self.cipher_util.encrypt(certificate.key)
+        data = {**certificate.dict(), "cert": cert, "key": key}
+        self.db_client.put_item(data)
 
     def list(self) -> List[Certificate]:
         response = self.db_client.scan_items()
@@ -32,7 +37,11 @@ class CertificateRepositoryDynamoDB(CertificateRepository):
                 f"CERTIFICATE_REPOSITORY :: CERTIFICATE_NOT_FOUND :: ID {id}"
             )
 
-        return Certificate(**item)
+        certificate = Certificate(**item)
+        certificate.cert = self.cipher_util.decrypt(certificate.cert)
+        certificate.key = self.cipher_util.decrypt(certificate.key)
+
+        return certificate
 
     def find_by_serial_number(self, serial_number: int) -> Certificate:
         response = self.db_client.query(
